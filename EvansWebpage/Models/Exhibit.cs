8e27f55f5d1d544ebe23f4588cad973b5e5177ae
Destroyere1107 <destroyere1107@outlook.com>
2026-04-20 21:43:59 -0600

    add multiple myCalcs links support

--- EvansWebpage/Models/Exhibit.cs
+++ EvansWebpage/Models/Exhibit.cs
@@ -4,21 +4,28 @@ namespace EvansWebpage.Models
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Category { get; set; }
+        public string Category { get; set; } 
         public string Manufacturer { get; set; }
         public string ManufacturerLogo { get; set; }
-        public string MainImageUrl { get; set; }
-        public string Model { get; set; }
-        public int? YearIntroduced { get; set; }
-        public string Type { get; set; }
-        public List<string> MyCalcsSearchIds { get; set; } = new List<string>();
         public string ManufacturerSlug { get; set; }
+        public string Model { get; set; }
         public string ModelSlug { get; set; }
+        public string Type { get; set; }
+        public int? YearIntroduced { get; set; }
+        public string MainImageUrl { get; set; } 
         public bool UnderConstruction { get; set; }
+        
+        public List<MyCalcsLink> MyCalcsLinks { get; set; } = new List<MyCalcsLink>(); 
         public List<Specimen> Specimens { get; set; } = new List<Specimen>();
         public List<GalleryImage> Gallery { get; set; } = new List<GalleryImage>();
     }
     
+    public class MyCalcsLink
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
     public class GalleryImage
     {
         public string Url { get; set; }
